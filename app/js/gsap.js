349850e540522480cb2618afe6dd8aa94dd2d60c
Erys Mozo <mozoerys@gmail.com>
2022-05-28 22:43:51 +0700

    commit

--- app/js/gsap.js
+++ app/js/gsap.js
@@ -1,4 +1,4 @@
 const timeline = gsap.timeline({defaults: {duration: 1, ease: "power2.easeInOut"}})
 timeline
     .fromTo(".hero__image", {x: "100%", opacity: 0}, {x: "0%", opacity: 1})
-    .from(".hero__contents",  {opacity: 0})
+    .To(".hero__contents",  {opacity: 1})
